26de6044c310648bae0f4ec0f6453a0001d3e668
kfd <kfd_personal@163.com>
2023-11-16 22:12:48 +0800

    fix: stats.js ignore cdn

--- vite.config.ts
+++ vite.config.ts
@@ -27,11 +27,6 @@ export default defineConfig({
           var: 'ReactDOM',
           path: `https://cdn.staticfile.org/react-dom/18.2.0/umd/react-dom.production.min.js`,
           // path: `https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js`
-        },
-        {
-          name: 'stats.js',
-          var: 'Stats',
-          path: 'https://cdnjs.cloudflare.com/ajax/libs/stats.js/r17/Stats.min.js'
         }
       ]
     }),
