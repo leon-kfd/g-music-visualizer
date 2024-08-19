035075e0a6cdd495cfe4e6068b256d495635b181
kfd <kfd_personal@163.com>
2024-08-19 14:47:56 +0800

    fix: default img source

--- src/global.ts
+++ src/global.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_IMG = `https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1459749411175-04bf5292ceea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=320&h=320&q=80`
+export const DEFAULT_IMG = `https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=320&h=320&q=80`
